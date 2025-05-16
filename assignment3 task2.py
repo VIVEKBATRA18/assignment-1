d844376fcaabe58de41c55b77656664575536f88
VIVEKBATRA18 <vivekbatra9816@gmail.com>
2025-05-16 15:07:54 +0530

    Add files via upload

---
+++ assignment3 task2.py
@@ -0,0 +1,19 @@
+a = int(input("enter the number:"))
+from math import *
+b = sqrt(a)
+c = log(a)
+d = sin(a)
+
+print ("Square root :",b)
+print ("logarithm is :",c)
+print ("Sin is :",d)
+
+
+
+
+
+
+
+
+
+
